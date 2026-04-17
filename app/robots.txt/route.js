420f9a5bf9a1f4e1d9a2b99f6d406db17962e78f
Rahim Siddiqui <rahimsiddiqui122@gmail.com>
2026-04-18 01:40:28 +0500

    Changes sitemap => sitemap_v2

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -9,7 +9,7 @@ Disallow: /api
 Disallow: /admin
 Disallow: /settings
 
-Sitemap: https://financialatelier.vercel.app/sitemap.xml
+Sitemap: https://financialatelier.vercel.app/sitemap_v2.xml
 `.trim();
 
   return new Response(robots, {
